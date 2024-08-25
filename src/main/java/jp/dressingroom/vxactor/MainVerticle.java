c63b591463ddf59cb393af75ab7e0976f846f956
Masatoshi Itoh <masatoshi9953@gmail.com>
2024-08-25 23:38:39 +0900

    Fixed: Now this can interact with vxcamel

--- src/main/java/jp/dressingroom/vxactor/MainVerticle.java
+++ src/main/java/jp/dressingroom/vxactor/MainVerticle.java
@@ -2,11 +2,16 @@ package jp.dressingroom.vxactor;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+    vertx.setPeriodic(5000, handler -> {
+      vertx.eventBus().send("out", JsonObject.of("k1", "v1"));
+    });
+
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
